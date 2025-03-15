5e195b9dc05132f25998e3a2e79caed0709e518b
Miguel-Deniss <baiozin.denisovas@gmail.com>
2025-03-15 13:22:00 -0300

    Create ex10.py

---
+++ PythonEx/ex10.py
@@ -0,0 +1,10 @@
+# Concatenação e repetição com operadores aritiméticos
+
+concatenacao = 'Miguel' + 'Denisovas'
+print(concatenacao)
+
+a_vezes = 'A' * 5
+print(a_vezes)
+
+nome_vezes = 3 * 'Miguel'
+print(nome_vezes)
